c33d41443bfd567875535f7c5485436731f837ad
Peter Fischer <peterfischer1990@gmail.com>
2016-11-12 16:51:20 +0100

    init

---
+++ test.py
@@ -0,0 +1,22 @@
+ 
+import sys
+import time
+import telepot
+
+def handle(msg):
+    content_type, chat_type, chat_id = telepot.glance(msg)
+    print(content_type, chat_type, chat_id)
+
+    if content_type == 'text':
+        out = 'Nein, du bist hier der ' + msg['text'] + ', lieber ' + msg['chat']['first_name']
+        bot.sendMessage(chat_id, out)
+	
+TOKEN = sys.argv[1]  # get token from command-line
+
+bot = telepot.Bot(TOKEN)
+bot.message_loop(handle)
+print ('Listening ...')
+
+# Keep the program running.
+while 1:
+    time.sleep(10)
